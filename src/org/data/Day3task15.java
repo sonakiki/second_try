390bc21284e3d947830d25594c8d748c078b3420
sonakiki <ksonamadesh96@gmail.com>
2025-05-21 00:19:51 +0530

    task15 done

--- src/org/data/Day3task15.java
+++ src/org/data/Day3task15.java
@@ -1,5 +1,17 @@
 package org.data;
 
-public class Day3task15 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Day3task15 {
+public static void main(String[] args)throws InterruptedException {
+		
+		
+		WebDriver driver =new ChromeDriver();
+		driver.navigate().to("https://www.youtube.com/");
+		
+       driver.findElement(By.xpath("//div[@class='yt-spec-touch-feedback-shape__fill']")).click();		
+}
+		
 }
